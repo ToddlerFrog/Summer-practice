5b633f7e5e7a31f666c7b0380d483b2d78427e91
ToddlerFrog <153516171+ToddlerFrog@users.noreply.github.com>
2025-08-31 11:39:00 +0700

    обновленная бд с правками

--- test.sql
+++ test.sql
@@ -11,6 +11,13 @@ create table if not exists visitor
  check(date_of_birth <= current_date)
 );
 
+create table if not exists benefits
+(
+ id_benefits integer,
+ name_benefits text not null,
+ primary key (id_benefits) 
+);
+
 create table if not exists ticket
 (
  number_ticket integer,
@@ -19,14 +26,15 @@ create table if not exists ticket
  date_visit date not null,
  id_benefits integer,
  primary key (number_ticket),
+ FOREIGN KEY (id_visitor) 
+    REFERENCES visitor(id_visitor)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE
+ FOREIGN KEY (id_benefits) 
+    REFERENCES benefits(id_benefits)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE
  check(date_visit >= current_date)
-);
-
-create table if not exists benefits
-(
- id_benefits integer,
- name_benefits text not null,
- primary key (id_benefits) 
 );
 
 create table if not exists exhibit 
@@ -45,6 +53,10 @@ create table if not exists photo_exhibit
  id_photo integer,
  photo text not null,
  primary key (id_exhibit, id_photo)
+ FOREIGN KEY (id_exhibit) 
+    REFERENCES exhibit(id_exhibit)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE
 );
 
 create table if not exists author
@@ -61,6 +73,14 @@ create table if not exists author_exhibit
  id_exhibit integer,
  id_author integer,
  primary key (id_exhibit, id_author)
+ FOREIGN KEY (id_exhibit) 
+    REFERENCES exhibit(id_exhibit)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE
+ FOREIGN KEY (id_author) 
+    REFERENCES author(id_author)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE
 );
 
 create table if not exists exposition
@@ -70,6 +90,10 @@ create table if not exists exposition
  descreption text not null,
  id_hall integer not null,
  primary key (id_exposition)
+ FOREIGN KEY (id_hall) 
+    REFERENCES hall(id_hall)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE 
 );
 
 create table if not exists photo_exposition
@@ -78,6 +102,10 @@ create table if not exists photo_exposition
  id_photo integer,
  photo text not null,
  primary key (id_exposition, id_photo)
+ FOREIGN KEY (id_exposition) 
+    REFERENCES exposition(id_exposition)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE
 );
 
 create table if not exists exhibit_in_exposition
@@ -85,6 +113,21 @@ create table if not exists exhibit_in_exposition
  id_exposition integer,
  id_exhibit integer,
  primary key (id_exposition, id_exhibit)
+ FOREIGN KEY (id_exposition) 
+    REFERENCES exposition(id_exposition)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE
+ FOREIGN KEY (id_exhibit) 
+    REFERENCES exhibit(id_exhibit)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE
+);
+
+create table if not exists floor_museum
+( 
+ number_floor integer, 
+ name_floor text,
+ primary key (number_floor)
 );
 
 create table if not exists hall
@@ -94,13 +137,32 @@ create table if not exists hall
  name_hall text,
  number_floor integer not null,
  primary key (id_hall)
+ FOREIGN KEY (number_floor) 
+    REFERENCES floor_museum(number_floor)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE
 );
 
-create table if not exists floor_museum
+create table if not exists category 
 ( 
- number_floor integer, 
- name_floor text,
- primary key (number_floor)
+ id_category integer,
+ name_category text not null,
+ primary key (id_category)
+);
+
+create table if not exists administrator 
+( 
+ id_admin integer,
+ first_name text not null,
+ second_name text not null,
+ surname text,
+ id_category integer not null,
+ confirmation_code integer not null,
+ primary key (id_admin)
+ FOREIGN KEY (id_category) 
+    REFERENCES category(id_category)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE
 );
 
 create table if not exists change_log
@@ -113,24 +175,21 @@ create table if not exists change_log
  id_admin integer not null,
  reason text not null,
  primary key (id_change)
+ FOREIGN KEY (id_exposition) 
+    REFERENCES exposition(id_exposition)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE
+ FOREIGN KEY (id_admin) 
+    REFERENCES administrator(id_admin)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE
 );
 
-create table if not exists administrator 
+create table if not exists status
 ( 
- id_admin integer,
- first_name text not null,
- second_name text not null,
- surname text,
- id_category integer not null,
- confirmation_code integer not null,
- primary key (id_admin)
-);
-
-create table if not exists category 
-( 
- id_category integer,
- name_category text not null,
- primary key (id_category)
+ id_status integer,
+ name_status text not null,
+ primary key (id_status)
 );
 
 create table if not exists status_exposition 
@@ -140,14 +199,33 @@ create table if not exists status_exposition
  start_date date, 
  end_date date,
  primary key (id_exposition, id_status)
+ FOREIGN KEY (id_exposition) 
+    REFERENCES exposition(id_exposition)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE
+ FOREIGN KEY (id_status) 
+    REFERENCES status(id_status)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE
 );
 
-create table if not exists status
-( 
- id_status integer,
- name_status text not null,
- primary key (id_status)
-);
+-- CREATE INDEX idx_ticket_visitor ON ticket(id_visitor);
+-- CREATE INDEX idx_ticket_benefits ON ticket(id_benefits);
+-- CREATE INDEX idx_photo_exhibit_exhibit ON photo_exhibit(id_exhibit);
+-- CREATE INDEX idx_author_exhibit_exhibit ON author_exhibit(id_exhibit);
+-- CREATE INDEX idx_author_exhibit_author ON author_exhibit(id_author);
+-- CREATE INDEX idx_exposition_hall ON exposition(id_hall);
+-- CREATE INDEX idx_photo_exposition_exposition ON photo_exposition(id_exposition);
+-- CREATE INDEX idx_exhibit_in_exposition_exposition ON exhibit_in_exposition(id_exposition);
+-- CREATE INDEX idx_exhibit_in_exposition_exhibit ON exhibit_in_exposition(id_exhibit);
+-- CREATE INDEX idx_hall_floor_museum  ON hall(number_floor);
+-- CREATE INDEX idx_change_log_floor_museum ON change_log(id_exposition);
+-- CREATE INDEX idx_change_log_administrator ON change_log(id_admin);
+-- CREATE INDEX idx_administrator_category ON administrator(id_category);
+CREATE INDEX idx_status_exposition_exposition ON status_exposition (id_exposition);
+CREATE INDEX idx_status_exposition_status ON status(id_status);
+
+
 
 -- INSERT INTO author (first_name, second_name, surname) VALUES ("Leonardo", "Da Vinchi", NULL), ("Ivan", "Shishkin", "Ivanovich"), ("Vasiliy", "Condinskiy", "Vasilievich"), ("William", "Van Gogh", NULL);
 -- INSERT INTO floor_museum (number_floor, name_floor) VALUES (1, "CLassical Art"), (2, "Modern Art"), (3, "Art of 19-20 centuries");
@@ -161,3 +239,4 @@ create table if not exists status
 -- ("Morning in the pine forest", "This painting by Ivan Ivanovich Shishkin was created in 1889. It is one of the most popular works by the artist and one of the most famous landscapes in the history of Russian art.", 2, 2);
 -- INSERT INTO photo_exhibit (id_exhibit, id_photo, photo) VALUES (1, 1, "images/car7.jpg"), (2, 2, "images/p3.jpg");
 -- INSERT INTO photo_exposition (id_exposition, id_photo, photo) VALUES (1, 1, "images/Cand.jpg"), (2, 2, "images/expos.jpg");
+
